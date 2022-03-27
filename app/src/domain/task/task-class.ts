aaf212954feaebb7689ec495e23e3f46ac2686cc
MsakiHoshino <right1121@hotmail.co.jp>
2022-03-28 00:00:06 +0900

    工数式追加

--- app/src/domain/task/task-class.ts
+++ app/src/domain/task/task-class.ts
@@ -1,10 +1,55 @@
+// enum TaskStatus {
+//   Ready = 0,
+//   InProgress = 1,
+//   Done = 2,
+//   Cancel = 3,
+// }
+type TaskStatus = 'Ready' | 'InProgress' | 'Done' | 'Cancel'
+
 export default class Task {
     id: number;
 
     title: string;
 
+    status: TaskStatus = 'Ready';
+
+    /** 最短工数(分) */
+    minManMinutes = 0;
+
+    /** 最長工数(分) */
+    maxManMinutes = 0;
+
+    /** 実績作業時間(分) */
+    actualWork = 0;
+
     constructor(title: string) {
       this.id = Date.now();
       this.title = title;
+    }
+
+    /** 見積工数(分) */
+    get manMinutes() {
+      return this.calcManMinutes();
+    }
+
+    /** バッファ */
+    get buffer() {
+      return this.calcBuffer();
+    }
+
+    /**
+     * 見積工数を計算する
+     * @returns 見積工数
+     */
+    calcManMinutes() {
+      return this.minManMinutes + this.calcBuffer();
+    }
+
+    /**
+     * バッファ時間を計算する
+     * @returns バッファ時間(分)
+     */
+    calcBuffer() {
+      return (this.maxManMinutes - this.minManMinutes) / 2;
     }
 }
