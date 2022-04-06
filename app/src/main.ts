2d5b3b9d3afb6a3df9ae4e7e5e1a8881b602e65f
MsakiHoshino <right1121@hotmail.co.jp>
2022-04-06 23:42:22 +0900

    firebase追加

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import { initializeApp } from 'firebase/app';
 import awesome from './awesome';
 import App from './App.vue';
 import './registerServiceWorker';
@@ -9,6 +10,18 @@ import './validate';
 import './assets/scss/global.scss';
 
 awesome();
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+};
+initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false;
 
 new Vue({
